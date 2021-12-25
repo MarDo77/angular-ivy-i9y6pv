b7bd20dd83d43bb1ee23692034bf6e128ca8b631
MarDo77 <mdomaslawski@gmail.com>
2021-12-25 16:21:29 +0100

    3 files modified, 12 added, 5 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+//import { Routes, RouterModule } from '@angular/router';
+import { appRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { MyCoins } from '../coins/coins.component';
+import { MyCoins } from './coins/coins.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent, MyCoins],
+  imports: [BrowserModule, FormsModule, HttpClientModule, appRoutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    MyCoins,
+    AboutComponent,
+    HomeComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
